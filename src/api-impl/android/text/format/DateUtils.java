d6d2c94abd8c10f56ca1d6e471e11f3b7120523e
Julian Winkler <julian.winkler1@web.de>
2024-11-25 10:01:51 +0100

    implement DateUtils.formatElapsedTime()
    This is needed for NewPipe 0.27.3

--- src/api-impl/android/text/format/DateUtils.java
+++ src/api-impl/android/text/format/DateUtils.java
@@ -16,4 +16,21 @@ public class DateUtils {
 		
 		return d1.getYear() == d2.getYear() && d1.getMonth() == d2.getMonth() && d1.getDate() == d2.getDate();
 	}
+
+	public static String formatElapsedTime(long elapsedMillis) {
+		final long days = elapsedMillis / (24 * 60 * 60L);
+		final long hours = elapsedMillis / (60 * 60L) % 24L;
+		final long minutes = elapsedMillis / 60L % 60L;
+		final long seconds = elapsedMillis % 60L;
+
+		if (elapsedMillis < 0) {
+			return "0:00";
+		} else if (days > 0) {
+			return String.format("%d:%02d:%02d:%02d", days, hours, minutes, seconds);
+		} else if (hours > 0) {
+			return String.format("%d:%02d:%02d", hours, minutes, seconds);
+		} else {
+			return String.format("%d:%02d", minutes, seconds);
+		}
+	}
 }
