b087b82616864b0545b44f99a6186a6a72774df0
Julian Winkler <julian.winkler1@web.de>
2024-12-10 23:48:50 +0100

    implement WallpaperManager.setBitmap() using XDG Portal

--- src/api-impl/android/app/WallpaperManager.java
+++ src/api-impl/android/app/WallpaperManager.java
@@ -1,5 +1,18 @@
 package android.app;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+
 public class WallpaperManager {
+
+	public static WallpaperManager getInstance(Context context) {
+		return new WallpaperManager();
+	}
+
+	public void setBitmap(Bitmap bitmap) {
+		set_bitmap(bitmap.pixbuf);
+	}
+
+	private static native void set_bitmap(long pixbuf);
 	
 }
