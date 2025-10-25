78cec693515e04de3d3bf60f2353b82b88f3d01b
Julian Winkler <julian.winkler1@web.de>
2025-10-25 06:57:57 +0200

    implement ShapeDrawable as constant color

--- src/api-impl/android/graphics/drawable/ShapeDrawable.java
+++ src/api-impl/android/graphics/drawable/ShapeDrawable.java
@@ -1,17 +1,29 @@
 package android.graphics.drawable;
 
+import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.drawable.shapes.Shape;
 
 public class ShapeDrawable extends Drawable {
 
+	private Paint paint = new Paint();
+
+	public ShapeDrawable() {}
+
 	public ShapeDrawable(Shape shape) {}
 
-	public Paint getPaint() {return new Paint();}
+	public Paint getPaint() {
+		return paint;
+	}
 
 	public void setPadding(Rect padding) {}
 
 	public void setShape(Shape shape) {}
+
+	@Override
+	public void draw(Canvas canvas) {
+		canvas.drawRect(getBounds(), paint);
+	}
 
 }
