582d4dcda197f961dc0a0e24c873146f748bc5de
Mis012 <Michael.Srba@seznam.cz>
2026-03-06 22:12:33 +0100

    main: pre-grow stack to 6MiB before ART slaps on a guard page
    On musl, ART slaps a guard page right after the default 128KiB
    stack, which therefore can't grow anymore. We want a larger stack,
    so we have to force the kernel to enlarge the mapping before
    we start the VM.

--- src/api-impl-jni/defines.h
+++ src/api-impl-jni/defines.h
@@ -5,6 +5,10 @@
 
 #define DEG2RAD(deg)    ((deg) * M_PI / 180)
 
+#define KiB 1024
+#define MiB 1024 * KiB
+#define GiB 1024 * MiB
+
 #define __likely__(x)   __builtin_expect(x, 1)
 #define __unlikely__(x) __builtin_expect(x, 0)
 
