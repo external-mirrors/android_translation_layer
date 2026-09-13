3e686d147bc3663b48750fee63d7ae05f1024288
Julian Winkler <julian.winkler1@web.de>
2026-09-13 13:08:28 +0000

    MediaMetadata.writeToParcel(): write one bundle for compatibility with MediaMetadataCompat

--- src/api-impl/android/media/MediaMetadata.java
+++ src/api-impl/android/media/MediaMetadata.java
@@ -1,6 +1,7 @@
 package android.media;
 
 import android.graphics.Bitmap;
+import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable.Creator;
 
@@ -9,6 +10,11 @@ public class MediaMetadata {
 	public static final Creator<MediaMetadata> CREATOR = new Creator<MediaMetadata>() {
 		@Override
 		public MediaMetadata createFromParcel(Parcel source) {
+			try {
+				source.readBundle();
+			} catch (ReflectiveOperationException e) {
+				throw new RuntimeException(e);
+			}
 			return new MediaMetadata();
 		}
 
@@ -37,5 +43,8 @@ public class MediaMetadata {
 		}
 	}
 
-	public void writeToParcel(Parcel dest, int flags) {}
+	public void writeToParcel(Parcel dest, int flags) {
+		// Must write exactly one bundle, so that ExoPlayer can read it into a MediaMetadataCompat
+		dest.writeBundle(Bundle.EMPTY);
+	}
 }
